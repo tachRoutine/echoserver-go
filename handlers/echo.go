76c7382e23659769f395a8f0f7164ab41b07d3b6
tacherasasi <tacherasasi@gmail.com>
2025-07-10 17:12:48 +0300

    Ellie: more chaos

--- handlers/echo.go
+++ handlers/echo.go
@@ -3,7 +3,10 @@ package handlers
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+
+	errorshandler "github.com/tacheraSasi/tripwire/errorsHandler"
 )
 
 type  EchoRequest struct{
@@ -23,12 +26,27 @@ func Echo(w http.ResponseWriter, r *http.Request){
 				message,
 			}
 		default:
-			return 
+			return
 	}
-	
+
 	response := map[string]string{"echo": echoReq.message}
 	fmt.Println(response)
 	w.Header().Set("Content-Type","application/json")
 	json.NewEncoder(w).Encode(response)
-	
+
+}
+
+func echoPost(w http.ResponseWriter,r *http.Request){
+	body, err := io.ReadAll(r.Body)
+	errorshandler.Check(err, "failed to read the body--->Post")
+
+	defer r.Body.Close()
+	var data map[string]string
+
+	if err := json.Unmarshal(body, &data); err != nil{
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+	return data
+
 }
