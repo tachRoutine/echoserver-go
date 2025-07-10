6ad66cfa22b23a8a2d2bf1c3a6f415c71122811c
tacherasasi <tacherasasi@gmail.com>
2025-07-10 13:35:57 +0300

    Ellie: returning the borin response

--- handlers/echo.go
+++ handlers/echo.go
@@ -1,7 +1,32 @@
 package handlers
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type  EchoRequest struct{
+	message string
+}
+
+type EchoResponse struct{
+	echo string
+}
 
 func Echo(w http.ResponseWriter, r *http.Request){
+	var echoReq EchoRequest;
+	switch r.Method{
+		case http.MethodGet:
+			message := r.URL.Query().Get("message")
+			echoReq = EchoRequest{
+				message,
+			}
+		default:
+			return 
+	}
+	
+	response := EchoResponse{echo: echoReq.message}
+	w.Header().Set("Content-Type","json")
+	json.NewEncoder(w).Encode(response)
 	
 }
